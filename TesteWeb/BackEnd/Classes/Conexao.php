e8cd4704226830d4fdde59eb6a7e3a670a54aaee
Eduardo dos Santos <edsanfiguesil@yahoo.com.br>
2017-07-21 18:47:32 -0300

    Atualização do aquivo Conexão.php

--- TesteWeb/BackEnd/Classes/Conexao.php
+++ TesteWeb/BackEnd/Classes/Conexao.php
@@ -1,14 +1,14 @@
 <?php
     class Conexao{
-        private $host = "mysql:host=www.sanfig.com.br;dbname=sanfi601_testeValemobi;charset=utf8";
-        private $usr = "sanfi601_valemob";
-        private $pwd = "*@teste@valemobi@*";
+        private $host = "";
+        private $usr = "";
+        private $pwd = "";
         
         
         /*
-        private $host = "mysql:host=127.0.0.1;dbname=dbmercadorias;charset=utf8";
-        private $usr = "carwash";
-        private $pwd = "*ed15@ed91*";
+        private $host = "";
+        private $usr = "";
+        private $pwd = "";
         */
         public function conectar(){
             return new PDO($this->host, $this->usr, $this->pwd);
